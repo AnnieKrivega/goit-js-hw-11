5da9b53118028010639b40fc70504bd2b25a1079
anniekrivega <121309759+AnnieKrivega@users.noreply.github.com>
2023-06-04 16:02:27 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,139 +1,74 @@
-import axios from "axios";
+
+
+import ImgApi from "./imgApi";
+
 import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+const imgApi = new ImgApi()
 
-const API_KEY = '37016253-52bbdbb3a453e4173ac0bf432';
-const BASE_URL = 'https://pixabay.com/api/';
+// function onFetch(name){
+//     return fetch(`https://pixabay.com/api/?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`).then(res =>{
+//         if(!res.ok){
+//             throw new Error(res.status)
+//          }      
+//     return res.json()}).then(res => {console.log(res); return res}).catch(err => console.log(err));
+// }
 
-const guard = document.querySelector('.js-guard');
+function forEachCard({webformatURL, largeImageURL,tags,likes,views,comments,downloads}){
+    
+return `<div class="photo-card"><img class="card-img" src="${webformatURL}" alt="${tags}" loading="lazy" /><div class="info"><p class="info-item"><b>Likes: ${likes}</b></p><p class="info-item"><b>Views: ${views}</b></p><p class="info-item"><b>Comments: ${comments}</b></p><p class="info-item"><b>Downloads: ${downloads}</b></p></div></div>`
 
-let options = {
-    root: null,
-    rootMargin: "330px",
-    threshold: 0,
-}
-let observer = new IntersectionObserver(handlerPagination, options);
-
-
-let page = 1;
-let totalHits = 0;
-const perPage = 40; 
-
-
-const lightbox = new SimpleLightbox('.gallery a');
-
-refs.form.addEventListener('submit', hendlerSearchForm);
-
-
-/**========================================================================
- * function hendler on form
- * @param {*string} event 
- */
-async function hendlerSearchForm(event) {
-    event.preventDefault();
-
-
-    clearListCards();
-
-    page = 1; //+
-
-    await serviceSearchImages();
 }
 
-async function serviceSearchImages() {
+const refs = {
+    formEl: document.querySelector('.search-form'),
+    cardListEl: document.querySelector('.gallery'),
+    loadMoreEl: document.querySelector('.load-more'),
+    
+}
 
-    const inputValue = refs.inputSearch.value.trim();
-    console.log(inputValue)
+refs.formEl.addEventListener('submit', onSubmit);
+refs.loadMoreEl.addEventListener('click', onLoadMore);
 
-    const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(inputValue)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
-    console.log(inputValue)
-
-    try {
-        const response = await axios.get(url);
-
-        const { data } = response;
-        const imagesAll = data.hits;
-        totalHits = data.totalHits;
-
-        if (imagesAll.length === 0) {
-            if (page === 1) {
-                Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again!')
-            }
-        } else {
-            Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`)
-            renderDataImage(imagesAll);
-        }
-        if (imagesAll.length < totalHits) {
-            observer.observe(guard);
-        } else if (imagesAll.length === totalHits) {
-            Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
-            observer.unobserve(guard);
-        }
-    } catch (error) {
-        console.log(error)
+function onSubmit(evt){
+    evt.preventDefault()
+    refs.loadMoreEl.classList.add('is-hidden')
+    clearHtml();
+    imgApi.query = refs.formEl.searchQuery.value;
+    if(imgApi.query===""){
+        refs.loadMoreEl.classList.add('is-hidden')
+        return
     }
+    
+console.log(refs.formEl.searchQuery.value)
+imgApi.resetPage();
+
+
+
+let onFetchCardList = imgApi.onFetch(name).then(r => { if(r.hits.length === 0){
+    Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    throw new Error();
+}
+Notiflix.Notify.success(`Hooray! We found ${r.totalHits} images.`);
+return r.hits.map( r => forEachCard(r))}
+    
+     ).then(r => {return r.join('')}).then(res => {addHtml(res); refs.loadMoreEl.classList.remove('is-hidden')} ).catch(err => console.log(err))
+
+console.log(onFetchCardList)
 }
 
-
-/**========================================================================
- * function for create  markup 
- * @param {*object} image 
- */
-function createMarkup(image) {
-    return `
-    <div class="photo-card">
-        <div class="gallery">
-            <a class="gallery__link" href="${image.largeImageURL}">
-                <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width="335"/>
-            </a>
-        </div>
-        <div class="info">
-            <p class="info-item"><b>Likes: </b>${image.likes}</p>
-            <p class="info-item"><b>Views: </b> ${image.views}</p>
-            <p class="info-item"><b>Comments: </b>${image.comments}</p>
-            <p class="info-item"><b>Downloads: </b>${image.downloads}</p>
-        </div>
-    </div>`;
+function onLoadMore(){
+   console.log(imgApi.onFetch().then(r => { if(r.hits.length === 0){
+    Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+    refs.loadMoreEl.classList.add('is-hidden')}; return r.hits.map( r => forEachCard(r))}
+    
+     ).then(r => {return r.join('')}).then(addHtml)) 
 }
 
-
-/**==========================================================================
- * function for render data from array in markup
- * @param {string} array 
- */
-function renderDataImage(array) {
-    const cardsImage = array.map(image => createMarkup(image)).join('');
-
-    refs.listCard.insertAdjacentHTML('beforeend', cardsImage);
-
-    const { height: cardHeight } = refs.listCard.firstElementChild.getBoundingClientRect();
-    window.scrollBy({
-        top: cardHeight * 0.4,
-        behavior: "smooth",
-    });
-
-    lightbox.refresh();
+function addHtml (html){
+refs.cardListEl.insertAdjacentHTML('beforeend', html)
 }
 
-
-/**==========================================================================
- * function for clear list with cards 
- */
-function clearListCards() {
-    refs.listCard.innerHTML = '';
+function clearHtml (){
+    refs.cardListEl.innerHTML = '';
 }
 
-
-/**===========================================================================
- * function callback by Intersection observer
- * @param {*params} entries
- */
-async function handlerPagination(entries) {
-    entries.forEach(async entry => {
-        if (entry.isIntersecting && page * perPage < totalHits) {
-            page += 1;
-            await serviceSearchImages(page);
-        }
-    })
-}
