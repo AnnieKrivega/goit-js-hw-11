7f33b0e003018618a23687958df48bb40a2c78df
anniekrivega <121309759+AnnieKrivega@users.noreply.github.com>
2023-06-05 21:43:50 +0300

    add

--- src/index.js
+++ src/index.js
@@ -52,12 +52,13 @@ console.log(onFetchCardList)
 
 
 function onLoadMore(){
-    if (hits.length < 40) {button.style.display = none}
+
 console.log(imgApi.onFetch().then(r => { if(r.hits.length === 0){
     Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
     refs.loadMoreEl.classList.add('is-hidden')}; return r.hits.map( r => forEachCard(r))}
     
     ).then(r => {return r.join('')}).then(addHtml)) 
+    if (hits.length < 40) {button.style.display = none}
 }
 
 
